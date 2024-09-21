1319f4ecaab94b69ca6c6f059fa75ace277acc92
mrmalvi <malviyak00@gmail.com>
2024-09-21 17:59:32 +0530

    add validation

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,10 @@ class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
   belongs_to :hospital
+  before_validation :set_defaults
+
+  private
+    def set_defaults
+      self.hospital ||= doctor&.hospital
+    end
 end
