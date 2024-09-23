1fd1f4c665e8d0033c5da227561bec35421a2c86
mrmalvi <malviyak00@gmail.com>
2024-09-23 11:04:31 +0530

    add graph

--- app/controllers/doctors/dashboards_controller.rb
+++ app/controllers/doctors/dashboards_controller.rb
@@ -1,6 +1,15 @@
 module Doctors
   class DashboardsController < BaseController
     def show
+      @all_appointments = begin
+        patients = current_hospital.patients.group("date(patients.created_at)").count.sort.to_h
+        {
+          dates: patients.keys,
+          series: [
+            {name: "Patients", data: patients.values}
+          ]
+        }
+      end
     end
   end
 end
